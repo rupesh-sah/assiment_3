6c98690524a40c3c9d0cf4f8b8332e21f11cb7ac
Rupesh sah <80407037+rupesh-sah@users.noreply.github.com>
2022-08-28 21:01:55 +0545

    Add files via upload

---
+++ assiment_3.py
@@ -0,0 +1,50 @@
+                    # Assignment-3: Type Conversion
+
+#(Q.1) Write a python script to convert a number into str type.
+from unicodedata import decimal
+
+
+num1 = 1234
+strr = str(num1)
+print(strr)
+
+#(Q.2) Write a python script to print Unicode of the character ‘m’
+
+unicordd = ord("m")
+print(unicordd)
+
+#(Q.3) Write a python script to print character representation of a given unicode 100.
+
+x = 100
+p = chr(x)
+print(p)
+
+#(Q.4) Write a python script to print any number and its binary equivalent
+a = 10
+bnr = bin(a)
+print(bnr)
+
+# (Q.5)Write a python script to print any number and its octal equivalent.
+b  = 20
+octs = oct(20)
+print(octs)
+
+#(Q.6) Write a python script to print any number and its hexadecimal equivalent.
+c = 30
+heg = hex(c)
+print(heg)
+
+'''(Q.7) Write a python script to store binary number 1100101 in a variable and print it in
+ decimal format.'''
+
+# d = 1100101
+# decs =decimal(d)
+# print(decs)
+
+'''(Q.9). Write a python script to store an octal number 125 in a variable and print it in binary
+format.'''
+
+octd = 125
+s = bin(octd)
+print(s)
+
